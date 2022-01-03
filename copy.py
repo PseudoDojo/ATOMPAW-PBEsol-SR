9697b385486ee538fbf6adcead9f9d6244259e63
jusong.yu <jusong.yeu@gmail.com>
2022-01-03 13:43:14 +0100

    copy std to str

--- copy.py
+++ copy.py
@@ -95,10 +95,28 @@ elements = ['Ag',
 #for ii in np.arange(100):
 #  os.system('mkdir '+str(elements[ii]))
 #  os.system('mv '+str(elements[ii])+'.*  '+str(elements[ii])+'/')
+#  os.system('mv '+str(elements[ii])+'.*  '+str(elements[ii])+'/')
+
+# for ii in np.arange(100):
+#   os.system('cd '+str(elements[ii])+'/ && git mv '+str(elements[ii])+'.GGA_PBE-JTH.xml '+str(elements[ii])+'_std.xml && cd ../')
 
 for ii in np.arange(100):
-#  os.system('cd '+str(elements[ii])+'/ && git mv '+str(elements[ii])+'.GGA_X_PBE_SOL+GGA_C_PBE_SOL-paw.xml '+str(elements[ii])+'.GGA-PBESOL-paw.xml && cd ../')
-  os.system('cd '+str(elements[ii])+'/ && git mv '+str(elements[ii])+'.GGA-PBESOL-JTH.xml '+str(elements[ii])+'_std.xml && cd ../')
-  os.system('cd '+str(elements[ii])+'/ && git mv '+str(elements[ii])+'.GGA-PBESOL-JTH.UPF '+str(elements[ii])+'_std.upf && cd ../')
+  exec_str = 'cd '+str(elements[ii])+'/ && cp '+str(elements[ii])+'_std.upf '+str(elements[ii])+'_str.upf && cd ../'
+  # print(exec_str)
+  os.system(exec_str)
+  
+  exec_str = 'cd '+str(elements[ii])+'/ && cp '+str(elements[ii])+'_std.xml '+str(elements[ii])+'_str.xml && cd ../'
+  os.system(exec_str)
 
+# cp ../jth-lda-upf/PBE-sp-UPF/As.LDA-PW-paw.UPF As/As_str.upf 
+# cp ../jth-lda-upf/PBE-sp-UPF/Bi.LDA-PW-paw.UPF Bi/Bi_str.upf 
+# cp ../jth-lda-upf/PBE-sp-UPF/In.LDA-PW-paw.UPF In/In_str.upf 
+# cp ../jth-lda-upf/PBE-sp-UPF/Pb.LDA-PW-paw.UPF Pb/Pb_str.upf 
+# cp ../jth-lda-upf/PBE-sp-UPF/Sb.LDA-PW-paw.UPF Sb/Sb_str.upf 
+# cp ../jth-lda-upf/PBE-sp-UPF/Sn.LDA-PW-paw.UPF Sn/Sn_str.upf 
+# cp ../jth-lda-upf/PBE-sp-UPF/Tl.LDA-PW-paw.UPF Tl/Tl_str.upf
 
+# for ii in np.arange(100):
+#   exec_str = 'cd '+str(elements[ii])+'/ && cp ../../paw_pw_stringent/'+str(elements[ii])+'.xml '+str(elements[ii])+'_str.xml && cd ../'
+#   # print(exec_str)
+#   os.system(exec_str)
